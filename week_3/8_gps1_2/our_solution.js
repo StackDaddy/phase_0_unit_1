4224ef4d4a2aafe98bea559e881a9a4cebcca2e1
Ryan <rms332@yahoo.com>
2014-04-15 17:09:11 -0600

    Update our_solution.js

--- week_3/8_gps1_2/our_solution.js
+++ week_3/8_gps1_2/our_solution.js
@@ -1,22 +1,34 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1.  Ryan stack 
+//  2.  Yariv 
 
 
 // 1. "YOU SIGNED... WHO?!"
 
-
+var client1 = {"name": "Matt Damon", "age": 40, "quote": "I'm great"};
+var client2 = {"name": "Johny Depp", "age": 45, "quote": "I'm weird"};
 
 
 // 2. "Here they Come!"
 
+var client3 = {"name": "Adam Sandler", "age": 56, "quote": "Last person to tell"};
+var client4 = {"name": "Kristen Bell", "age": 33, "quote": "Do you wanna build a snowman?"};
+var client5 = {"name": "Jim Carrey", "age": 52, "quote": "You're telling me there is a chance"};
 
-
+var clientele = [client1, client2, client3, client4, client5];
 
 
 // 3. "TIME IS MONEY!"
+
+function Client(name, age, quote) {
+    this.name = name;
+    this.age = age; 
+    this.quote = quote;
+};
+
+
 
 //YOUR CODE HERE!
 
@@ -32,7 +44,7 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 // 4. "SHOW 'EM OFF!"
 
 
-
+clientele
 
 // ** BONUS **
 
