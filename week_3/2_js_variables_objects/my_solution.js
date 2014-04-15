f2ae488d6504f74dba6c04781a017b10ec3dfdf2
Ryan <rms332@yahoo.com>
2014-04-15 13:48:43 -0600

    Update my_solution.js

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -25,14 +25,20 @@ members = ["John", 4, 5, "Mary"];
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+members = ["John", 4, 5, "Mary"];
 
+For the variable names, I sumply used what the test was asking for.  Additionally, I added in several semicolons, creating proper syntax.
 
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
+
+// I never had done test driven development before.  I had heard the name tossed around a couple of times, but this was the first time writing any code for it.  Overall, assuming I did the challenge correctly, I found it to be pretty straight forward.  I have had some practice declaring variables in Javascript using Coderbyte, so this seemed pretty straight forward.  the only thing that I forgot was adding the semicolons at the end of the variable declaration, which is proper syntax.  
 // 
 // 
 // 
