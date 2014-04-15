15036c75df8e100dbe80a96aad62e9f74551d38f
Ryan <rms332@yahoo.com>
2014-04-15 07:50:38 -0600

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -5,7 +5,61 @@
 // command + / This will comment the line. 
  
  
+ 2)  Grab the Mushroom
  
+     pseudocode:  Move up; Move Right, Grab the Mushroom, Move left, Move up, Attack!
+     
+this.moveUp();
+
+this.moveRight();
+ 
+this.moveLeft();
+
+this.moveUp();
+
+this.attackNearbyEnemy();
+
+3) Drink Me Complete 
+pseudocode:  Bob and Weave,headfake, slay left, double tap, retrace steps, go downtown and grab health portion, go uptown, turn right and slay again.
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveDown();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+4)Taunt the Gaurds
+pseudoCode:  Move right, say something, turn around, kill gaurd, move right, up, say something, down, right, up, right, right.
+this.moveRight();
+this.bustDownDoor();
+
+// Delete the "//" in front of each line below.
+this.moveRight();
+this.say("Hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+
+// Now get Phoebe to follow through the dungeon.
+this.moveRight();
+this.say("Follow me.");
+
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.say("follow me.");
+this.moveRight(); 
+this.say("Run");
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+
+
+
  
  
  
