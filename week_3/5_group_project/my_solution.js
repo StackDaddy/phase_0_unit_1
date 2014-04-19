b1af1af5341ba8e2d98ce52f562952cef755b7c4
Ryan <rms332@yahoo.com>
2014-04-19 10:22:24 -0600

    Update my_solution.js

--- week_3/5_group_project/my_solution.js
+++ week_3/5_group_project/my_solution.js
@@ -1,10 +1,14 @@
+//I want to ask a person how many numbers they want to enter, and once they tell me how many, I would like ot ask
+// them which numbers.  I will then store these numbers.  If a person tells me something other than a number, 
+//I would like to remind them to tell me a number.  
+
 var array = [];
 
 var counter = prompt("How many numbers do you want to enter?");
 
 var counter = parseInt(counter);
 
-while  (typeof counter != "number") 
+while (typeof counter != "number") 
 {
   var counter = prompt("Please enter a number:");
 }
@@ -19,7 +23,7 @@ for (x = 0; x < counter; x++){
 	}
 }
 
-
+// Here I would like to calculate the sum of all the numbers tht were inputed. 
 
 var sum = function(array){
 	var total = 0;
@@ -28,6 +32,8 @@ var sum = function(array){
 	}
 	return total;
 };
+
+//Here I would like to calculate the median of all the numbers that were inputed.  
 
 var median = function (array){
 	var temp = (array.length)/2;
@@ -38,10 +44,16 @@ var median = function (array){
 		return ((array[temp] + array[temp-1])/2);
 	}
 };
+
+//Here, I would like to calculate the mean of all the numbers that were inputed.
+
 var mean = function (array){
 	var avg = sum(array)/array.length;
 	return avg;
 };
+
+// Here, I would like to print out all the results of my calculations.  
+
 console.log(sum(array));
 console.log(median(array));
 console.log(mean(array));
